d17656933e884aaf59c4d3c1045b14b94462ab80
rapidotech <161102302+rapidotech@users.noreply.github.com>
2025-05-19 00:55:51 +0800

    Add files via upload

---
+++ AOOP - MotorPH from OOP/src/oop_motorph/OOP_MotorPH.java
@@ -0,0 +1,19 @@
+
+//MotorPH Payroll System
+//by: Joyce Ferrer, Ryu Ken Lindo, Mikko Jerome Bautista
+//Documentation: https://drive.google.com/drive/folders/1ITfc7In7laZnAmPle9DHLznQclhY0V7j
+
+
+package oop_motorph;
+
+
+public class OOP_MotorPH {
+
+   
+    public static void main(String[] args) {
+        
+        
+        new frm_Login().setVisible(true);
+        
+    }  
+}
